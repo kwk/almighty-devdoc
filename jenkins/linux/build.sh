733ee5ea44fbf41cadd042a3b2db70dfe913e69f
Konrad Kleine <kkleine@redhat.com>
2016-06-17 17:45:36 +0200

    Fix volume permission issues and speed-up iterative builds
    - To avoid issues with permissions of volumes mounted into
      a container we create some a user that has the same UID and
      username as the user that starts a container. Under this user
      all commands to build the software and install dependencies
      are executed.
    
      - This allows build artefacts to be deleted by a local or a
        jenkins user once the container terminates.
      - As a precaution a bash trap ensures that everything is
        owned by the user that started the container
    
    - To speed up iterative  builds I've moved the installation path
      for `bundle install` inside of build directoy that is mounted
      into the container on every start. If you don't call `make clean`
      the installed bundle will persist from each call to `make` to
      the next call to `make`.

--- jenkins/linux/build.sh
+++ jenkins/linux/build.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
-
-# Echo to stderr
-function errcho {
-  >&2 echo "====  $@  ====";
-}
+#!/usr/bin/env bash
 
 # Show all command prior to executing them
 set -x
 
 # Exit if a command fails
 set -e
+
+# Create user with same UID and name as the user that
+# started the container
+echo "Creating new user \"$USER\" with UID \"$USERID\""
+useradd -m ${USER} -u ${USERID}
 
 # Where the source is stored?
 # Take the value of the first and second argument or default
@@ -18,33 +18,18 @@ source_dir=${1:-/source}
 build_dir=${2:-/build}
 
 # Give all rights to users outside of the container
-function clean_up {
-  errcho "Cleaning up before exiting"
-  
-  errcho "Make build directory ${build_dir} readable from outside of container"
-  chmod o+rw -Rv ${build_dir}
-
-  # TODO (kkleine) Remove this bash when running in Jenkins
-  # it is just here for interactive debugging
-  bash
+function almighty_clean_up {
+  chown -R $USER ${build_dir} 
 }
-trap clean_up EXIT SIGHUP SIGINT SIGTERM SIGKILL
+trap 'echo "SIGNAL received. Will clean up."; almighty_clean_up' SIGUSR1 SIGTERM SIGINT EXIT
 
-# Enable RVM (hide commands)
-set +x
-source /etc/profile.d/rvm.sh
-set -x
-
-# Restore bash trap
-trap clean_up EXIT SIGHUP SIGINT SIGTERM SIGKILL
-
-cd ${source_dir}
-cp -Rfvp . ${build_dir}
-
-# Install all ruby gems as specified in the Gemfile
-cd ${build_dir}
-bundle install
-
-# Deploy
-bundle exec rake deploy
+su $USER --command=" \
+cd ${source_dir} 
+cp -Rfp . ${build_dir} 
+chown -R $USER ${build_dir} 
+source /etc/profile.d/rvm.sh \
+&& cd ${build_dir} \
+&& bundle install --path=${build_dir}/bundle-install \
+&& bundle exec jekyll build \
+"
 
